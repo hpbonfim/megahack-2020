3e6e3378bf545abd5bc09c0cfb51a3687c7543a5
Larissa Pissurno <larissapissurno@gmail.com>
2020-04-30 22:13:52 -0400

    Tela home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
 ];
 
 @NgModule({
