e43cf930f23b0e9362ec2ad2e6c4703773129ed5
Henrique Bonfim <hp_bonfim@hotmail.com>
2020-05-03 17:13:03 -0300

    backend v3.0.0

--- backend/api/routes/function.js
+++ backend/api/routes/function.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const router = express.Router()
 
+// CONTROLLER
 const contractController = require('../controllers/contracts')
 const verifyController = require('../controllers/verify')
 
-//CONTRACTS 
+//CONTRACTS ROUTES
 router.post("/encrypt", contractController.contract_encrypt)
 router.post("/decrypt", contractController.contract_decrypt)
 
-//SMS TWILIO
+//SMS TWILIO ROUTES
 router.post("/verify", verifyController.user_verify)
 router.post("/send", verifyController.user_sendVerification)
 
--- backend/database.config.js
+++ backend/database.config.js
@@ -1,4 +1,2 @@
 
-module.exports = {
-    url: 'mongodb://'+`${process.env.MONGO_USER}:${process.env.MONGO_PASS}`+'@'+`${process.env.MONGO_URL}`+':'+`${process.env.MONGO_PORT}`+`${process.env.MONGO_QUERY}`
-}
+module.exports = { url: 'mongodb://'+`${process.env.MONGO_USER}:${process.env.MONGO_PASS}`+'@'+`${process.env.MONGO_URL}`+':'+`${process.env.MONGO_PORT}`+`${process.env.MONGO_QUERY}` }
